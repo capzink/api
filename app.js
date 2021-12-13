bf8a543af2c4a4abbe3f5b29e300b25651c6c8b8
Carlos <capz2003@gmail.com>
2021-12-13 11:15:44 -0500

    added places route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = require('express')()
-
+import express from 'express'
+import routePlaces from ('./routes/places')
 const port = process.env.PORT || 3000
 
+
+app.use??(express.json())
+app.use('api/places', routePlaces)
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
